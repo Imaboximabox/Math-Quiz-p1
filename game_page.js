b7ee0c211984ccb6f3dd8364ecf12c99a099cc2b
Imaboximabox <96030087+Imaboximabox@users.noreply.github.com>
2022-07-04 16:56:07 -0700

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -8,3 +8,13 @@ document.getElementById("player1score").innerHTML = player1score
 document.getElementById("player2score").innerHTML = player2score
 document.getElementById("player1name").innerHTML = player1name
 document.getElementById("player2name").innerHTML = player2name
+function send(){
+    number1 = document.getElementById("number1").value
+    number2 = document.getElementById("number2").value
+    answer = number1*number2
+   questionword = "<h4 id='word_display'> Q. "+number1 +"X"+ number2+"</h4>";
+   inputbox = "<br>answer;<input id='inputcheckbox'>" 
+   checkbutton = "<br><br><button onclick='check()'>check</button>"
+   row = questionword+inputbox+checkbutton
+   document.getElementById("output").innerHTML = row
+}
